2a30e9bb1ec5b2046852e27104fa536a66dc3a53
dustpeo19 <sm18570@naver.com>
2021-08-04 00:19:45 +0900

    complete

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about_container">
+      <span>"lorem ipsum"</span>
+      <span>-Lorem</span>
+    </div>
+  );
+}
+
+export default About;
